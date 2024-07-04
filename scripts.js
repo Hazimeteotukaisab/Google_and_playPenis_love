527e23d03d90b3452afaf19425eb483322695a2f
Hazimeteotukaisab <168907207+Hazimeteotukaisab@users.noreply.github.com>
2024-07-04 22:50:00 +0900

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -43,9 +43,9 @@ async function verifyAuthCode() {
 
 async function getIpAddress() {
     try {
-        const response = await fetch('https://api.ipify.org?format=json');
-        const data = await response.json();
-        return data.ip;
+        const response = await fetch('https://ipinfo.io/ip');
+        const ip = await response.text();
+        return ip.trim();
     } catch (error) {
         console.error('Error fetching IP address:', error);
         return 'Unknown IP';
